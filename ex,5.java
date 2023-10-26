e20e87587a4cb98ea30ea9d74caf77c2b2fe3c83
AA21232 <144646591+AA21232@users.noreply.github.com>
2023-10-26 12:46:46 -0400

    Create ex,5.java

---
+++ ex,5.java
@@ -0,0 +1,30 @@
+
+public class App {
+    public static void main(String[] args) {
+     
+     int counter = 1;
+     String text = "text";
+
+     do{
+        System.out.println(text);
+        counter++;
+     }
+    
+     while(counter<= 5);
+
+
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
