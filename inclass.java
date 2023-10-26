28a4e55fe7fde4f5c5586cb31c1a35e8904ab220
AA21232 <144646591+AA21232@users.noreply.github.com>
2023-10-26 12:47:41 -0400

    Create inclass.java

---
+++ inclass.java
@@ -0,0 +1,62 @@
+class Main{ 
+    public static void main(String[] args){
+/* 
+        int i = 10; 
+        while (i < 16) {
+            System.out.println(i);
+            i++; //i=i+1 : i+=1 ; i++
+        }
+
+         int a = 15; 
+        while (a > 9) {
+            System.out.println(a);
+            a=a-1; //i=i+1 : i+=1 ; i++
+        }
+}
+        //for loop 
+        int n=10;
+        for(int j=1; j<=n; j++) 
+        {
+            System.out.println(j);
+        }
+
+        //int x=10; 
+
+        for(int y=2; y<=10; y+=2)
+        {
+            System.out.println(y);
+        }
+        int r=10; 
+        for(int h=1; h<=r; h+=2)
+        {
+            System.out.println(h);
+        }  
+        for(int l=10;l>=1;l-=2)
+        {
+            System.out.println(l);
+        }
+
+        int Sum=0; 
+        for(int i =0; i<11; i=i+2)
+        
+            Sum=Sum+i; //or Sum+=i  or i=i+2
+            System.out.println("sum of first 10 numbers:  "+Sum);
+
+        int i=15;
+        while(i<=30)
+        {
+
+            System.out.println(i);
+            i++;
+        }*/
+        int i=1; 
+        do
+        {
+            i++;
+            System.out.println(i);
+            
+        }
+        while(i<=10);
+    }
+
+}
