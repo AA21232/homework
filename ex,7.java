dd4cf5c2f21de040bc3319254ee4965234c83a7f
AA21232 <144646591+AA21232@users.noreply.github.com>
2023-10-26 12:49:03 -0400

    Create ex,7.java

---
+++ ex,7.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        
+     for(int i = 1; i<=1000; i++){
+        
+        if (i ==300){
+            break;
+        }
+        System.out.println(i);
+     }
+
+    
+}
+
+    
+
+
+}
+    
